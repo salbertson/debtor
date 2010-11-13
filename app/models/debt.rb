fd90c6a1560eaa61d000b388dd6ba0c518db534c
Scott Albertson <salbertson@ezpublishing.com>
2010-11-13 07:55:35 -0800

    Refactored #balance and added tests for Debt.

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -2,10 +2,6 @@ class Debt < ActiveRecord::Base
   has_many :payments, :dependent => :destroy
 
   def balance
-    payment_total = 0
-    payments.each do |payment|
-      payment_total += payment.amount
-    end
-    amount - payment_total
+    amount - payments.sum(:amount)
   end
 end
--- test/unit/debt_test.rb
+++ test/unit/debt_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class DebtTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @debt = Debt.create(:name => "Chevron", :amount => 100)
+
+    @payment1 = Payment.create(:amount => 10, :debt => @debt)
+    @payment2 = Payment.create(:amount => 10, :debt => @debt)
+  end
+
+  test "#balance" do
+    assert @debt.balance == 80
+  end
+
+  test "payments should be destroyed" do
+    @debt.destroy
+    assert !Payment.exists?(@payment1)
+    assert !Payment.exists?(@payment2)
   end
 end
